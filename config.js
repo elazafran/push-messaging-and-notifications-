19cbc00b43547a1a2a37a203213607ac2e1ae595
Javier Aliaga <javiealiaga@gmail.com>
2018-05-08 21:25:22 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAApL-C4zc:APA91bEeXafBUCOMjvnUvPHjmDMEBVpheaxvqe6dsa0Qgga8jdnHtmg1roeut3myhOiz19hiefT7UVMnEsbHHXxg3ZVDECqNFGoceUiRq30ZgXdWIung2rQG7qVXknFM-Fk-fMiCpcCn'
+};
